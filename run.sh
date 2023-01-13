592fc9eb8e87336e242d1b52976cc078987b182c
Murat Bilgehan Ertan <muratbilgehanertan@gmail.com>
2023-01-13 15:37:45 +0300

    add:run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-PROJECT_DIR=$PWD
+PROJECT_DIR="$PWD"
 BACKEND_DIR=$PROJECT_DIR/backend/node
 FRONTEND_DIR=$PROJECT_DIR/frontend
-GO_DIR=$PROJECT_DIR/go/cmd/micros
+GO_DIR=$PROJECT_DIR/backend/go/cmd/micros
 
-npm install -g pm2
+
+#npm install -g pm2
 cd $BACKEND_DIR && npm install
 cd $FRONTEND_DIR && yarn install
 
+cd $GO_DIR
+pm2 start go -- run .
+
 cd $BACKEND_DIR
 pm2 start index.js
-
-cd $GO_DIR
-echo $PWD
-pm2 start go -- run .
 
 cd $FRONTEND_DIR
 pm2 start yarn -- start
