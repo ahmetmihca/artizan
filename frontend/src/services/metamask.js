ee4b81a88ee580a7268a1e984b2de8d0ead371aa
Alper ARI <alper.hamza.ari@gmail.com>
2023-01-16 16:32:44 +0300

    Change market contract ABIs

--- frontend/src/services/metamask.js
+++ frontend/src/services/metamask.js
@@ -1,5 +1,5 @@
-import trade_services from './market_serv'
-const Web3 = require('web3');
+import trade_services from "./market_serv";
+const Web3 = require("web3");
 const web3 = new Web3(window.ethereum);
 let metamask = window.ethereum;
 
@@ -7,7 +7,6 @@ let walletConnected = false;
 checkConnection();
 
 const onLoginHandler = async () => {
-
   if (getWalletId() !== false) {
     return;
   }
@@ -16,10 +15,14 @@ const onLoginHandler = async () => {
     let accounts = await web3.eth.requestAccounts();
 
     if (accounts !== null) {
+      console.log("hello2");
+
       localStorage.setItem("wallet", accounts[0].toLowerCase());
+
       let isWhitelisted = await trade_services.isWhitelisted(accounts[0]);
-      localStorage.setItem("isWhitelisted",isWhitelisted)
-      console.log("isWhitelisted:"+isWhitelisted)
+      localStorage.setItem("isWhitelisted", isWhitelisted);
+      console.log("isWhitelisted:" + localStorage.getItem("isWhitelisted"));
+
       walletConnected = true;
       return accounts[0];
     }
@@ -30,36 +33,41 @@ function getWalletId() {
   checkConnection();
 
   let walletId = localStorage.getItem("wallet");
-  
+
   if (walletId == null) {
     return false;
   }
-  
+
   return walletId;
 }
 
 function checkConnection() {
-  metamask.request({ method: 'eth_accounts' })
+  metamask
+    .request({ method: "eth_accounts" })
     .then((e) => {
-      if(e.length == 0){
+      if (e.length == 0) {
         walletConnected = false;
-        localStorage.removeItem("wallet"); 
+        localStorage.removeItem("wallet");
       } else {
         walletConnected = true;
-        if(localStorage.getItem("wallet") == null)
-        {
-          localStorage.setItem("wallet", e[0])
+        if (localStorage.getItem("wallet") == null) {
+          localStorage.setItem("wallet", e[0]);
+          console.log("hello1");
+          // // let isWhitelisted = await trade_services.isWhitelisted(accounts[0]);
+          // let isWhitelisted = false;
+          // localStorage.setItem("isWhitelisted", isWhitelisted);
+          // console.log("isWhitelisted:" + isWhitelisted);
         }
       }
     })
-  .catch(console.error);
+    .catch(console.error);
 }
 
-window.ethereum.on('accountsChanged', function (accounts) {
+window.ethereum.on("accountsChanged", function (accounts) {
   checkConnection();
 });
 
-function checkWallet(){
+function checkWallet() {
   return walletConnected;
 }
 
@@ -67,11 +75,15 @@ function checkWallet(){
 async function signMessage(msg, wallet) {
   let result = await web3.eth.personal.sign(msg, wallet);
   return result;
-};
-
+}
 
 const metamask_operations = {
-  onLoginHandler, getWalletId, signMessage, checkConnection, checkWallet, metamask
+  onLoginHandler,
+  getWalletId,
+  signMessage,
+  checkConnection,
+  checkWallet,
+  metamask,
 };
 
 export default metamask_operations;
