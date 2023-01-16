4aaee6cebd314b57a5bc8ad481159392f9cd77db
ugurasikoglu <ugurasikoglu@sabanciuniv.edu>
2023-01-16 22:37:31 +0300

    Contract updated with verify
    Contract updated with verify and other functionalities.

--- contarcts/contracts/ArtizanMarket.sol
+++ contarcts/contracts/ArtizanMarket.sol
@@ -9,18 +9,140 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./ArtizanNFT.sol";
 
 contract ArtizanMarket is ReentrancyGuard {
+
+    
     using Counters for Counters.Counter;
-    Counters.Counter private _itemsID;
-    Counters.Counter private _itemsSold; // keep track how many sale happened
-
+    Counters.Counter private _listID;
     address payable owner;
-    uint256 listingPrice = 0.010 ether;
 
+    struct ListInfo {
+        uint256 nft_id;
+        uint256 price;
+        uint256 time; //in second
+        uint256 expire_date; // inital 0
+        address owner;
+        address seller;
+        string company;
+    }
+    mapping(uint256 => uint256) private tokenIDToListID;
+    mapping(uint256 => ListInfo) private ListIDToListInfo;
+    mapping(address => uint256[]) private usersTotokenID;
+
+
+    event Listing(
+        uint256 indexed tokenID,
+        uint256 time,
+        address seller,
+        uint256 price
+    );
+
+    event Buying(
+        uint256 nft_id,
+        uint256 time,
+        uint256 expire_date,
+        address owner
+    );
     constructor() {
         owner = payable(msg.sender);
-        addToWhitelist(payable(msg.sender), "contract_owner");
+        addToWhitelist(payable(msg.sender),payable(msg.sender) ,"contract_owner");
     }
 
+    function createMarketItem(
+        uint256 price,
+        uint256 nft_id,
+        uint256 time,
+        address nft_address,
+        string memory company
+    ) public payable nonReentrant {
+        require(price > 0, "Too low");
+        _listID.increment();
+        uint256 listID = _listID.current();
+        tokenIDToListID[nft_id] = listID;
+        ListIDToListInfo[listID] = ListInfo(
+            nft_id,
+            price,
+            time,
+            0,
+            payable(address(this)),
+            payable(msg.sender),
+            company
+        );
+        IERC721(nft_address).transferFrom(msg.sender, address(this), nft_id);
+        emit Listing(nft_id, time, msg.sender, price);
+    }
+
+    function createMarketSale(address nft_address, uint256 nft_id)
+        public
+        payable
+        nonReentrant
+    {
+        uint256 listID = tokenIDToListID[nft_id];
+        uint256 currentPrice = ListIDToListInfo[listID].price;
+        uint256 currentTokenId = ListIDToListInfo[listID].nft_id;
+        address seller = ListIDToListInfo[listID].seller;
+        require(msg.value >= currentPrice, "Please pay the asking price");
+
+        ListIDToListInfo[listID].owner = payable(msg.sender);
+        ListIDToListInfo[listID].seller = payable(address(0));
+        uint256 lifeTime = ListIDToListInfo[listID].time;
+        ListIDToListInfo[listID].expire_date = block.timestamp + lifeTime;
+        IERC721(nft_address).transferFrom(
+            address(this),
+            msg.sender,
+            currentTokenId
+        );
+        payable(seller).transfer(msg.value);
+        usersTotokenID[msg.sender].push(nft_id);
+        emit Buying(
+            nft_id,
+            ListIDToListInfo[listID].time,
+            ListIDToListInfo[listID].expire_date,
+            msg.sender
+        );
+    }
+
+    function verify(string memory companyToAccess) public view returns (bool) {
+        uint256 membershipNumber = usersTotokenID[msg.sender].length;
+        for (uint256 i = 0; i < membershipNumber; i++) {
+            uint256 tokenID = usersTotokenID[msg.sender][i];
+            uint256 listID = tokenIDToListID[tokenID];
+            string memory company = ListIDToListInfo[listID].company;
+            if (
+                keccak256(abi.encodePacked(company)) ==
+                keccak256(abi.encodePacked(companyToAccess))
+            ) {
+                uint256 currentBlock = block.timestamp;
+                uint256 expire_date = ListIDToListInfo[listID].expire_date;
+                if (expire_date >= currentBlock) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    function StopNFTSale(address NftCont, uint256 tokenId)
+        public
+        payable
+        nonReentrant
+    {
+        uint256 listID = tokenIDToListID[tokenId];
+        require(
+            msg.sender == ListIDToListInfo[listID].seller,
+            "Only item seller can perform this"
+        );
+        ListIDToListInfo[listID].seller = payable(address(0));
+        ListIDToListInfo[listID].owner = payable(msg.sender);
+
+        IERC721(NftCont).transferFrom(address(this), msg.sender, tokenId);
+    }
+
+    function NFTItem(uint256 tokenId) public view returns (ListInfo memory) {
+        uint256 item = tokenIDToListID[tokenId];
+        return ListIDToListInfo[item];
+    }
     // The contract defines a struct to represent an address on the
     // whitelist.
     struct WhitelistEntry {
@@ -29,20 +151,6 @@ contract ArtizanMarket is ReentrancyGuard {
         // The name of the whitelisted account.
         string name;
     }
-
-    struct MarketItem {
-        address nftContract;
-        uint256 tokenID;
-        address payable owner;
-        address payable seller;
-        uint256 price;
-        bool sold;
-        bool soldBefore;
-    }
-
-    mapping(uint256 => MarketItem) private idMarketItem;
-    mapping(uint256 => uint256) private tokenToItem;
-
     // The contract defines a mapping to store the whitelist entries.
     // The keys of the mapping are the whitelisted addresses, and the
     // values are the corresponding whitelist entries.
@@ -69,17 +177,33 @@ contract ArtizanMarket is ReentrancyGuard {
         // and false otherwise.
         return whitelist[account].account != address(0);
     }
+    function isOwner(address account) public view returns (bool) {
+        // Return true if the given address is on the whitelist,
+        // and false otherwise.
+        if(account==owner)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     // The contract defines a function to add a new address to the
     // whitelist. Only the contract owner is allowed to call this
     // function.
-    function addToWhitelist(address account, string memory name)
+    function addToWhitelist(address account, address caller, string memory name)
         public
-        onlyOwner
     {
         // Ensure that the given address is not already on the whitelist.
         require(
             whitelist[account].account == address(0),
             "Address is already on the whitelist."
+        );
+        require(
+            isOwner(caller),
+            "Caller is not the owner of the contract."
         );
 
         // Create a new whitelist entry for the given address.
@@ -97,271 +221,8 @@ contract ArtizanMarket is ReentrancyGuard {
         // Emit an event to indicate that the address was added to the whitelist.
         emit WhitelistAdded(account, name);
     }
-    modifier onlyOwner() {
-        require(
-            msg.sender == owner,
-            "Only owner of the marketplace can change this"
-        );
-        _;
-    }
     // Define the owner() function to return the contract owner.
     function getOwner() public view returns (address) {
         return owner;
-    }
-
-
-    event MarketItemCreation(
-        address nftContract,
-        uint256 indexed tokenID,
-        address owner,
-        address seller,
-        uint256 price,
-        bool sold,
-        bool soldBefore
-    );
-
-    function getListingPrice() public view returns (uint256) {
-        return listingPrice;
-    }
-
-    function updateListingPrice(uint256 _listingPrice) public payable {
-        require(
-            owner == msg.sender,
-            "Only marketplace owner can update listing price."
-        );
-        listingPrice = _listingPrice;
-    }
-
-    ////This function operates putting an NFT on the market which is not sold before, it is a new item.
-    function createMarketItem(
-        uint256 price,
-        address NftCont,
-        uint256 tokenId
-    ) public payable nonReentrant {
-        require(price > 0, "Too low");
-        require(
-            msg.value == listingPrice,
-            "Make sure you have the listing Price"
-        );
-        _itemsID.increment();
-        uint256 currentItemID = _itemsID.current();
-        tokenToItem[tokenId] = currentItemID;
-        idMarketItem[currentItemID] = MarketItem(
-            NftCont,
-            tokenId,
-            payable(address(this)),
-            payable(msg.sender),
-            price,
-            false,
-            false
-        );
-        IERC721(NftCont).transferFrom(msg.sender, address(this), tokenId);
-        emit MarketItemCreation(
-            NftCont,
-            tokenId,
-            address(this),
-            msg.sender,
-            price,
-            false,
-            false
-        );
-    }
-
-    ////This function operates putting an NFT on the market which is sold before, it is a new item.
-    function isSoldBefore(uint256 tokenId) public view returns (bool) {
-        uint256 item = tokenToItem[tokenId];
-        if (idMarketItem[item].soldBefore == true) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function ResellNFT(
-        uint256 price,
-        address NftCont,
-        uint256 tokenId
-    ) public payable nonReentrant {
-        uint256 item = tokenToItem[tokenId];
-        require(
-            idMarketItem[item].owner == msg.sender,
-            "Only item owner can perform this operation"
-        );
-        require(
-            msg.value == listingPrice,
-            "Price must be equal to listing price"
-        );
-
-        ArtizanNFT tokenContract = ArtizanNFT(NftCont);
-        tokenContract.transferToken(msg.sender, address(this), tokenId);
-
-        idMarketItem[item].sold = false;
-        idMarketItem[item].price = price;
-        idMarketItem[item].seller = payable(msg.sender);
-        idMarketItem[item].owner = payable(address(this));
-        _itemsSold.decrement();
-    }
-
-    ////This function operates stopping an NFT Sale
-    function StopNFTSale(
-        uint256 price,
-        address NftCont,
-        uint256 tokenId
-    ) public payable nonReentrant {
-        uint256 item = tokenToItem[tokenId];
-        require(
-            msg.sender == idMarketItem[item].seller || msg.sender == owner,
-            "Only item owner or seller can perform this"
-        );
-        /*
-        ArtizanToken tokenContract = ArtizanToken(NftCont);
-        tokenContract.transferToken(msg.sender, address(this), tokenId);
-        */
-        idMarketItem[item].sold = true;
-        idMarketItem[item].price = price;
-        idMarketItem[item].seller = payable(address(0));
-        idMarketItem[item].owner = payable(msg.sender);
-        idMarketItem[item].soldBefore = true;
-        _itemsSold.increment();
-
-        IERC721(NftCont).transferFrom(address(this), msg.sender, tokenId);
-    }
-
-    //// This function operates an NFT Sale/Transaction
-    function createMarketSale(address NftCont, uint256 tokenId)
-        public
-        payable
-        nonReentrant
-    {
-        uint256 item = tokenToItem[tokenId];
-        uint256 currentPrice = idMarketItem[item].price;
-        uint256 currentTokenId = idMarketItem[item].tokenID;
-        address seller = idMarketItem[item].seller;
-        require(msg.value >= currentPrice, "Please pay the asking price");
-        require(idMarketItem[item].sold == false, "Item has been already sold");
-
-        idMarketItem[item].owner = payable(msg.sender); //// market item i guncelle yeni sahip
-        idMarketItem[item].sold = true; ////market item i guncelle satildi
-        idMarketItem[item].seller = payable(address(0));
-        idMarketItem[item].soldBefore = true;
-        _itemsSold.increment(); //// toplam satisi guncelle
-        IERC721(NftCont).transferFrom(
-            address(this),
-            msg.sender,
-            currentTokenId
-        ); //// Bu contracttaki tokenId si currenttokenid olanin sahipligini msg.sender'a ver
-        payable(owner).transfer(listingPrice); // pazarcinin komisyonu (contract sahibi)
-        payable(seller).transfer(msg.value); //// NFT yi satan kisiye parayi gonder
-    }
-
-    //// This function operates listing NFTs on marketsale (unsold items)
-    function ListItemsOnSale() public view returns (MarketItem[] memory) {
-        uint256 itemCount = _itemsID.current();
-        uint256 itemUnsold = _itemsID.current() - _itemsSold.current();
-        uint256 currentindex = 0;
-
-        MarketItem[] memory items = new MarketItem[](itemUnsold);
-        for (uint256 index = 0; index < itemCount; index++) {
-            if (idMarketItem[index + 1].owner == address(this)) {
-                uint256 currentItemID = index + 1;
-                MarketItem storage currentItem = idMarketItem[currentItemID];
-                items[currentindex] = currentItem;
-                currentindex += 1;
-            }
-        }
-        return items;
-    }
-
-    /*
-    function buyNFT(uint256 tokenId) public payable nonReentrant {
-        uint256 item=tokenToItem[tokenId];
-        idMarketItem[tokenId].owner = payable(msg.sender); //// market item i guncelle yeni sahip
-        idMarketItem[tokenId].sold = true; ////market item i guncelle satildi
-        idMarketItem[tokenId].seller = payable(address(0));
-        idMarketItem[tokenId].soldBefore = true;
-        _itemsSold.increment();
-    }
-*/
-    //// This function operates User's NFTs (purchased)
-    function ListUserOwnItems() public view returns (MarketItem[] memory) {
-        uint256 totalItem = _itemsID.current();
-        uint256 itemCount = 0;
-        uint256 currentindex = 0;
-
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (idMarketItem[index + 1].owner == msg.sender) {
-                itemCount += 1;
-            }
-        }
-        MarketItem[] memory items = new MarketItem[](itemCount);
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (idMarketItem[index + 1].owner == msg.sender) {
-                uint256 currentItemID = index + 1;
-                MarketItem storage currentItem = idMarketItem[currentItemID];
-                items[currentindex] = currentItem;
-                currentindex += 1;
-            }
-        }
-        return items;
-    }
-
-    //// This function operates User's listed NFTs (on sale)
-    function ListUserSaleItems() public view returns (MarketItem[] memory) {
-        uint256 totalItem = _itemsID.current();
-        uint256 itemCount = 0;
-        uint256 currentindex = 0;
-
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (idMarketItem[index + 1].seller == msg.sender) {
-                itemCount += 1;
-            }
-        }
-        MarketItem[] memory items = new MarketItem[](itemCount);
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (idMarketItem[index + 1].seller == msg.sender) {
-                uint256 currentItemID = index + 1;
-                MarketItem storage currentItem = idMarketItem[currentItemID];
-                items[currentindex] = currentItem;
-                currentindex += 1;
-            }
-        }
-        return items;
-    }
-
-    function ListUsersAllItems(address user)
-        public
-        view
-        returns (MarketItem[] memory)
-    {
-        uint256 totalItem = _itemsID.current();
-        uint256 itemCount = 0;
-        uint256 currentindex = 0;
-
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (
-                idMarketItem[index + 1].seller == user ||
-                idMarketItem[index + 1].owner == user
-            ) {
-                itemCount += 1;
-            }
-        }
-        MarketItem[] memory items = new MarketItem[](itemCount);
-        for (uint256 index = 0; index < totalItem; index++) {
-            if (
-                idMarketItem[index + 1].seller == user ||
-                idMarketItem[index + 1].owner == user
-            ) {
-                uint256 currentItemID = index + 1;
-                MarketItem storage currentItem = idMarketItem[currentItemID];
-                items[currentindex] = currentItem;
-                currentindex += 1;
-            }
-        }
-        return items;
-    }
-
-    function NFTItem(uint256 tokenId) public view returns (MarketItem memory) {
-        uint256 item = tokenToItem[tokenId];
-        return idMarketItem[item];
     }
 }
